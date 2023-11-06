f36c3d000aaecfb5c1ab20457906b26f0891f6e6
KietDarealist <kietmakietna@gmail.com>
2023-11-06 19:33:25 +0700

    update node to 16

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
   env: {
-    "jest/globals": true,
+    'jest/globals': true,
   },
   root: true,
-  extends: ["@react-native-community"],
-  plugins: ["jest"],
+  plugins: ['jest'],
   rules: {
-    semi: ["error", "never"],
-    "object-curly-spacing": ["error", "always"],
-    "array-bracket-spacing": ["error", "never"],
-    "react/require-default-props": ["error"],
-    "react/default-props-match-prop-types": ["error"],
-    "react/sort-prop-types": ["error"],
+    semi: ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react/require-default-props': ['error'],
+    'react/default-props-match-prop-types': ['error'],
+    'react/sort-prop-types': ['error'],
   },
   settings: {
-    "import/resolver": {
-      "babel-module": {},
+    'import/resolver': {
+      'babel-module': {},
     },
   },
-};
+}
--- redux/index.ts
+++ redux/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { reducer } from './slices'
+import auth from './slices/auth'
+import cart from './slices/cart'
+import filter from './slices/filter'
 
 const store = configureStore({
-  reducer,
-})
+  reducer: { auth: auth, cart: cart, filter: filter },
+}) as any
 
 export type IRootState = ReturnType<typeof store.getState>
 export type IStoreDispatch = typeof store.dispatch
