d7c96c3f2f1f074c4eec78bfa9878104ca721711
KietDarealist <kietmakietna@gmail.com>
2024-04-06 14:06:33 +0700

    fix payment reducer is not initalize

--- src/redux/rootReducers.ts
+++ src/redux/rootReducers.ts
@@ -3,13 +3,14 @@ import auth from './slices/auth'
 import cart from './slices/cart'
 import filter from './slices/filter'
 import category from './slices/category'
+import payment from './slices/payment'
 
 const rootReducer = combineReducers({
   auth: auth,
   cart: cart,
   filter: filter,
   category: category,
+  payment: payment,
 }) as any
-
 
 export default rootReducer
