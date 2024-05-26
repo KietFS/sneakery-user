23c489bf94d48a89578ec9cc48ef4eb45c488a1b
Kiet Huynh <kiet.huynh@mercatus.com>
2024-05-26 12:13:10 +0700

    fix env

--- src/components/molecules/HorizontalProductCard/index.tsx
+++ src/components/molecules/HorizontalProductCard/index.tsx
@@ -13,38 +13,30 @@ const HorizontalProductCard: React.FC<IHorizontalProductCardProps> = props => {
   const { product } = props
   const router = useRouter()
 
-  return (
-    <a
-      href={`${
-        process.env.NODE_ENV === 'production'
-          ? `https://sneakery.vercel.app/products/${product.id}`
-          : `http://localhost:3000/products/${product.id}`
-      }`}
-    >
-      <div
-        className=" border-b border-gray-200 px-2 py-2 flex flex-col gap-y-5 w-full cursor-pointer hover:opacity-80"
-        onClick={() => {}}
-      >
-        <div className="flex gap-x-3 items-center">
-          <img src={product.imagePath} width={80} height={80} />
-          <div className="flex flex-col gap-y-2">
-            <p className="text-sm text-gray-600 font-semibold">
-              {product.name}
-            </p>
+  console.log('NODE ENV', process.env.NODE_ENV)
 
-            <p className="text-xs text-gray-600">
-              {product.startPrice?.toString().prettyMoney()}
-            </p>
-            <div className="flex items-center gap-x-1">
-              <p className="text-xs text-gray-600">Bán bởi:</p>
-              <div className="rounded-full bg-blue-200 text-blue-800 font-semibold px-[5px] py-[2px] text-[8px] w-fit">
-                {product.userName}
-              </div>
+  return (
+    <div
+      className=" border-b border-gray-200 px-2 py-2 flex flex-col gap-y-5 w-full cursor-pointer hover:opacity-80"
+      onClick={() => router.replace(`/products/${product.id}`)}
+    >
+      <div className="flex gap-x-3 items-center">
+        <img src={product.imagePath} width={80} height={80} />
+        <div className="flex flex-col gap-y-2">
+          <p className="text-sm text-gray-600 font-semibold">{product.name}</p>
+
+          <p className="text-xs text-gray-600">
+            {product.startPrice?.toString().prettyMoney()}
+          </p>
+          <div className="flex items-center gap-x-1">
+            <p className="text-xs text-gray-600">Bán bởi:</p>
+            <div className="rounded-full bg-blue-200 text-blue-800 font-semibold px-[5px] py-[2px] text-[8px] w-fit">
+              {product.userName}
             </div>
           </div>
         </div>
       </div>
-    </a>
+    </div>
   )
 }
 
